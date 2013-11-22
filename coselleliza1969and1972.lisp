a3aa44ebcdfddc69849b818da075b8e7cbec5abb
Jeff Shrager <jshrager@stanford.edu>
2013-11-22 14:36:22 -0800

    Upcase for consistency (as pew did in the transcript)

--- coselleliza1969and1972.lisp
+++ coselleliza1969and1972.lisp
@@ -371,55 +371,55 @@
 
 ;;; /docfns/   31 July 1969  1007:42
 
-(test
-  (lambda (d s)
-    (prog (cd psv)
-	  (setq psv (cdr parselist))
-      lp  (cond
-	    ((null d)
-	      (cond
-	        (s (go rn))
-		(t (setq parselist (car parselist))
-		  (return t))))
-	    ((eq 0 (setq cd (car d)))
-	      (go t0))
-	    ((null s)
-	      (go rn))
-	    ((numberp cd)
-	      (tconc s parselist)
-	      (cond
-	        ((setq s (nth s cd))
-		  (go t3))
-	        (t (go rn))))
-            ((atom cd)
-	      (cond
-	        ((eq cd (car s)))
-		(t (go rn))))
-	    ((car cd)
-	      (cond
-	        ((member (car s)
-	            cd))
-		(t (go rn))))
-	    ((test4 (car s)
-		    (cdr cd)))
-	    (t (go rn)))
-    	  (tconc s parselist)
-      t3  (setq s (cdr s))
-          (setq d (cdr d))
-	  (go lp)
-      t0  (tconc s parselist)
-          (cond
-	    ((null (setq d (cdr d)))
-	      (setq parselist (car parselist))
-	      (return t)))
-      t1  (cond
-	    ((test d s)
-	      (return t))
-	    ((setq s (cdr s))
-	      (go t1)))
-      rn  (rplacd parselist (cond
-	      (psv (rplacd psv nil))))
-          (return nil)
+(TEST
+  (LAMBDA (D S)
+    (PROG (CD PSV)
+	  (SETQ PSV (CDR PARSELIST))
+      LP  (COND
+	    ((NULL D)
+	      (COND
+	        (S (GO RN))
+		(T (SETQ PARSELIST (CAR PARSELIST))
+		  (RETURN T))))
+	    ((EQ 0 (SETQ CD (CAR D)))
+	      (GO T0))
+	    ((NULL S)
+	      (GO RN))
+	    ((NUMBERP CD)
+	      (TCONC S PARSELIST)
+	      (COND
+	        ((SETQ S (NTH S CD))
+		  (GO T3))
+	        (T (GO RN))))
+            ((ATOM CD)
+	      (COND
+	        ((EQ CD (CAR S)))
+		(T (GO RN))))
+	    ((CAR CD)
+	      (COND
+	        ((MEMBER (CAR S)
+	            CD))
+		(T (GO RN))))
+	    ((TEST4 (CAR S)
+		    (CDR CD)))
+	    (T (GO RN)))
+    	  (TCONC S PARSELIST)
+      T3  (SETQ S (CDR S))
+          (SETQ D (CDR D))
+	  (GO LP)
+      T0  (TCONC S PARSELIST)
+          (COND
+	    ((NULL (SETQ D (CDR D)))
+	      (SETQ PARSELIST (CAR PARSELIST))
+	      (RETURN T)))
+      T1  (COND
+	    ((TEST D S)
+	      (RETURN T))
+	    ((SETQ S (CDR S))
+	      (GO T1)))
+      RN  (RPLACD PARSELIST (COND
+	      (PSV (RPLACD PSV NIL))))
+          (RETURN NIL)
       )))
 
 ;;; Eliza-19690731-DOCFNSp1-04of06
