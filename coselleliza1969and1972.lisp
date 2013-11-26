jeffshrager/elizagen.org

commit 841266dba4328e93bef02c6271167efa17ad0d53

Add RPLACD / RPLACA

coselleliza1969and1972.lisp
@@ -66,7 +66,7 @@
 (defpackage :bbn-lisp
   (:use :cl)
   (:nicknames :bl)
-  (:shadow :cons :nth :prin1 :cdr )
+  (:shadow :cons :cdr :rplacd :rplaca :nth :prin1)
   (:export :defineq :setqq :rplqq :tconc :clock :put :getp
            :quotient :spaces :remainder :plus :minus :pack :greaterp :ratom :pctlis :trmlis
            ;; Also export Symbols named for CL symbols
@@ -122,18 +122,31 @@
 (defun getp (sym prop)
   (getf (bl:cdr sym) prop))
 
-;;; BL:CDR of a symbol is (symbol-plist symbol),
-;;; and similarly in rplcd'ing symbols.
-
-(defun cdr (x)
-  (cond ((consp x) (cl:cdr x))
-	((symbolp x) (symbol-plist x))))
-
 ;;; BBN Lisp CONS creates a cons cell, like (list nil) or (cons nil
 ;;; nil), but (CL:CONS) is an error, so we define (BL:CONS)
 
 (defun cons (&optional car cdr)
   (cl:cons car cdr))
+
+;;; BL:CDR of a symbol is (symbol-plist symbol),
+;;; and similarly in rplcd'ing symbols.
+
+(defun cdr (x)
+  (cond ((consp x) (cl:cdr x))
+	((symbolp x) (symbol-plist x))
+        (t (error "CDR expected a cons or a symbol"))))
+
+;;; TODO: Should we have all C[AD]+R variations?
+
+(defun rplacd (x y)
+  (cond ((consp x) (cl:rplacd x y))
+        ((symbolp x) (setf (symbol-plist x) y))
+        (t (error "RPLACD expected a cons or a symbol"))))
+
+(defun rplaca (x y)
+  (cond ((consp x) (cl:rplaca x y))
+        ((symbolp x) (setf (symbol-value x) y))
+        (t (error "RPLACA expected a cons or a symbol"))))
 
 (defun nth (x n)
   (nthcdr n (cons nil x)))

commit 5e65c3934cc6b77dae87a9c87435f920885e58b1

Allow CDR of a number (fixes #7)
Eliza takes the CDR of a number. This is actually legal in BBN-Lisp (see page
5.1 of the Apr69 manual). Commit 841266d broke this by introducing an error
branch in our CDR implementation. Solution: add an explicit NUMBERP test.

This change also broke (CDR NIL), because NIL is not a cons. Change the test
from CONSP to LISTP.

(CONSP is correct for RPLACA and RPLACD, as modifying NIL is not allowed, even
in BBN Lisp (page 3.4))

coselleliza1969and1972.lisp
@@ -136,21 +136,22 @@
 ;;; and similarly in rplcd'ing symbols.
 
 (defun cdr (x)
-  (cond ((consp x) (cl:cdr x))
+  (cond ((listp x) (cl:cdr x))
 	((symbolp x) (symbol-plist x))
-        (t (error "CDR expected a cons or a symbol"))))
+        ((numberp x) nil)
+        (t (error "CDR expected a list, a symbol or a number: ~W" x))))
 
 ;;; TODO: Should we have all C[AD]+R variations?
 
 (defun rplacd (x y)
   (cond ((consp x) (cl:rplacd x y))
         ((symbolp x) (setf (symbol-plist x) y))
-        (t (error "RPLACD expected a cons or a symbol"))))
+        (t (error "RPLACD expected a cons or a symbol: ~W" x))))
 
 (defun rplaca (x y)
   (cond ((consp x) (cl:rplaca x y))
         ((symbolp x) (setf (symbol-value x) y))
-        (t (error "RPLACA expected a cons or a symbol"))))
+        (t (error "RPLACA expected a cons or a symbol: ~W" x))))
 
 (defun nth (x n)
   (nthcdr n (cons nil x)))